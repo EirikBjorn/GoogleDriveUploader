6f7df35e51857ae17801e8ba1a9e09b77a41deb4
Eirik Bjørneseth <bjornesetheirik@gmail.com>
2023-01-24 17:41:29 +0100

    change date format and del block

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from pydrive2.drive import GoogleDrive
 from pydrive2.auth import GoogleAuth
-from datetime import date
+from datetime import timedelta, date
 import os
 import time
 
 gauth = GoogleAuth()
 
 # Try to load saved client credentials
-gauth.LoadCredentialsFile("mycreds.txt")
+gauth.LoadCredentialsFile("/home/pi/Documents/vods/mycreds.txt")
 if gauth.credentials is None:
     # Fix refresh error by forcing offline
     gauth.GetFlow()
@@ -22,20 +22,12 @@ else:
     # Initialize the saved creds
     gauth.Authorize()
 # Save the current credentials to a file
-gauth.SaveCredentialsFile("mycreds.txt")
+gauth.SaveCredentialsFile("/home/pi/Documents/vods/mycreds.txt")
 
 drive = GoogleDrive(gauth)
 
 path = r"/home/pi/Documents/vods/vid"
-i = 1
-for file in os.listdir(path):
-  newFile = drive.CreateFile({'title': f"{date.today()}--wubby-{i}", 'parents': [{'id': '1n1eeqQH4mr1J_gv_-d_cp1sqAj0nYNl_'}]})
-  newFile.SetContentFile(os.path.join(path, file))
-  newFile.Upload()
-  print(f" {file} has been uploaded to Google Drive")
-  i+=1
-  f = None
-  
+
 for file in os.listdir(path):
   age = os.stat(os.path.join(path, file)).st_mtime
   if age < time.time() - 86400:
@@ -43,3 +35,13 @@ for file in os.listdir(path):
     os.remove(os.path.join(path, file))
   else:
     print(f" {file} is too young to be deleted")
+
+i = 1
+for file in os.listdir(path):
+  newFile = drive.CreateFile({'title': f"{date.today() - timedelta(days=1)}--wubby-{i}", 'parents': [{'id': '1n1eeqQH4mr1J_gv_-d_cp1sqAj0nYNl_'}]})
+  newFile.SetContentFile(os.path.join(path, file))
+  newFile.Upload()
+  print(f" {file} has been uploaded to Google Drive")
+  i+=1
+  f = None
+  
