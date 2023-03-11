57cdb51291e4db30f66aeff0fd0e88cb9d33dae8
Eirik Bjørneseth <bjornesetheirik@gmail.com>
2023-03-11 11:25:49 +0100

    Keep files for 2 weeks, only upload if younger than 24 hours

--- main.py
+++ main.py
@@ -28,20 +28,23 @@ drive = GoogleDrive(gauth)
 
 path = r"/home/pi/Documents/vods/vid"
 
+# Delete all files older than 24 hours
 for file in os.listdir(path):
   age = os.stat(os.path.join(path, file)).st_mtime
-  if age < time.time() - 86400:
+  if age < time.time() - (86400 * 14):
     print(f" Deleted: {file}")
     os.remove(os.path.join(path, file))
   else:
     print(f" {file} is too young to be deleted")
 
+# Create, name and upload file to google drive
 i = 1
 for file in os.listdir(path):
-  newFile = drive.CreateFile({'title': f"{date.today() - timedelta(days=1)}--wubby-{i}", 'parents': [{'id': '1n1eeqQH4mr1J_gv_-d_cp1sqAj0nYNl_'}]})
-  newFile.SetContentFile(os.path.join(path, file))
-  newFile.Upload()
-  print(f" {file} has been uploaded to Google Drive")
-  i+=1
-  f = None
+  if age > time.time() - 86400:
+    newFile = drive.CreateFile({'title': f"{date.today() - timedelta(days=1)}--wubby-{i}", 'parents': [{'id': '1n1eeqQH4mr1J_gv_-d_cp1sqAj0nYNl_'}]})
+    newFile.SetContentFile(os.path.join(path, file))
+    newFile.Upload()
+    print(f" {file} has been uploaded to Google Drive")
+    i+=1
+    f = None
   
